34ad6f7c5b29b77a5eddd88e79ea90fa2a03df3f
Bill <bill.kimtai@student.moringaschool.com>
2024-04-24 08:03:54 +0300

    completed

--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -1,12 +1,15 @@
 import React from "react";
-import ProjectItem from "./ProjectItem";
+// import ProjectItem from "./ProjectItem"; // Remove this line if not needed
 
 function ProjectList({ projects }) {
   console.log(projects);
+  const projectsList = projects.map((project) => (
+    <span key={project.id}>{project.name}</span>
+  ));
   return (
     <div id="projects">
       <h2>My Projects</h2>
-      <div id="project-list">{/* render ProjectItem components here */}</div>
+      <div id="project-list">{projectsList}</div>
     </div>
   );
 }
